faaf124d5dcf24334c1aeda2a10777ea5297be02
kushagra <kuku12320@gmail.com>
2020-08-30 00:38:24 +0530

    connected peers with 4 distinct peers

--- 1/peer.py
+++ 1/peer.py
@@ -1,7 +1,7 @@
 # code for peer when it wants to set up a TCP connection with a seed.
 
 from socket import *
-from utils import Connection, findSeeds
+from utils import *
 import random
 import json
 
@@ -9,6 +9,8 @@ encoding = 'utf-8'
 
 # max size of a message
 packet_size = 1024
+num_messages = 10
+message_time = 5
 
 seeds = findSeeds()
 N = len(seeds)
@@ -17,8 +19,10 @@ random.shuffle(seeds)
 seeds = seeds[:N//2+1]
 
 seed_list = []
+peer_list = []
 peers = []
 
+# connect to the selected seeds
 for (ip, port) in seeds:
     s = socket()
     s.connect((ip, port))
@@ -28,3 +32,13 @@ for (ip, port) in seeds:
     print("Peer list", current_peers)
     peers = peers + current_peers
     
+#select a max of 4 distinct peers to connect to
+peers = list(set(peers))
+peers = peers[:min(4,len(peers))]
+
+# connect to the selected peers
+for (ip, port) in peers:
+    s = socket()
+    s.connect((ip, port))
+    peer_list.append(Connection(s, ip, port))
+
