0aa20889aae5e6c4a92cac75445eba69f9fcb9e0
AnshKhurana <anshkhurana1507@gmail.com>
2020-09-03 16:49:49 +0530

    Refactored existing code.

---
+++ 1/peer_revised.py
@@ -0,0 +1,145 @@
+"""
+Usage: python peer.py
+TODO add custom config path
+
+Peer Script. Connects to at least half the seeds randomly and gets peer list
+Sends 10 example messages. Sends heartbeat every 13 seconds
+"""
+
+from socket import *
+from utils import *
+import random
+import json
+import time
+import os
+import selectors
+
+encoding = 'utf-8'
+
+# GLOBAL VARIABLES
+# max size of a message
+PACKET_SIZE = 1024
+NUM_MESSAGES = 10
+MESSAGE_TIME = 5
+
+class Peer:
+    def __init__(self):
+        super().__init__()
+
+        # get seeds        
+        seeds = findSeeds()
+        N = len(seeds)
+        # select n//2+1 seeds randomly
+        random.shuffle(seeds)
+        self.seeds = seeds[:N//2+1]
+
+        self.received_peer_list = []
+        self.received_from = 0
+        self.peers = []
+        self.sel = selectors.DefaultSelector()
+        
+        # set-up listening socket for the node.
+        self.listener_socket = socket(AF_INET, SOCK_STREAM)
+        # listens on a socket even if previously occupied
+        self.listener_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.listener_socket.bind(('', 0))
+        self.listener_socket.listen(10)
+        self.listener_socket.setblocking(False)
+        self.listening_port = self.listener_socket.getsockname()[1]
+        self.sel.register(self.listener_socket, selectors.EVENT_READ, data=Connection(
+            self.listener_socket, '', self.listening_port, socket_type.SELF))
+
+    def connect_with_seeds(self):
+        # connect to the selected seeds
+        for (ip, port) in self.seeds:
+            s = socket()
+            s.setblocking(False)
+            print("connecting to seed", ip, ":", port)
+            s.connect_ex((ip, port))
+            self.sel.register(s, selectors.EVENT_READ | selectors.EVENT_WRITE,
+                        data=Connection(s, ip, port, sock_type=socket_type.SEED))
+
+    def run(self):
+        while True:
+            events = self.sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    self.accept_peer(key.fileobj)
+                else:
+                    self.service_connection(key, mask)
+
+    def accept_peer(self, sock):
+        """
+        Accept connection from peer and add it to selector.
+        Dont send peer list as we haven't received listening port yet
+        """
+        # Haven't checked this function
+        peer, (peer_ip, peer_port) = sock.accept()
+        print("Received connection from", peer_ip, peer_port)
+        peer.setblocking(False)
+        self.sel.register(peer, selectors.EVENT_READ | selectors.EVENT_WRITE,
+                    data=Connection(peer, peer_ip, None))
+
+    def connect_with_peers(self):
+        
+        if len(self.received_peer_list) == 0:
+            print("No other peers in the network")
+
+        for (ip, port) in getUnique(self.received_peer_list):
+            s = socket()
+            s.setblocking(False)
+            print("connecting to peer", ip, ":", port)
+            s.connect_ex((ip, port))
+            self.sel.register(s, selectors.EVENT_READ | selectors.EVENT_WRITE,
+                        data=Connection(s, ip, port, sock_type=socket_type.PEER))
+
+        print("sent connection request to all?")
+
+        
+    def service_connection(self, key, mask):
+        """
+        Handle all requests.
+        Cases:
+            - We need to send listening port info to seed
+            - Seed has sent peer list. Add it to local peer list,
+            if n/2 seeds are registered, start connecting to other peers
+            - A peer has sent listening port. Add it to connected peers
+            - Handle gossip and liveness messages
+        """
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            try:
+                recv_data = sock.recv(PACKET_SIZE)  # Should be ready to read
+                if not recv_data:
+                    print("closing connection to", data.ip, ":", data.port)
+                    self.sel.unregister(sock)
+                    sock.close()
+                elif data.type == socket_type.SEED:  # get peer list
+                    rec = json.loads(recv_data.decode(encoding))
+                    print("received peer list from",
+                        data.ip, ":", data.port, ":", rec)
+                    self.received_peer_list += rec
+                    self.received_from += 1
+                    if self.received_from == len(self.seeds):
+                        print("Connecting to peers")
+                        self.connect_with_peers()
+                elif data.type == socket_type.PEER:
+                    print("received from peer", data.ip, ":", data.port, ":", recv_data)
+            except Exception as e:
+                print(e)
+                self.sel.unregister(sock)
+                sock.close()
+        
+        if mask & selectors.EVENT_WRITE:
+            if not data.sent_id:  # Send listening port info
+                print("sending listening info to", data.ip, ":", data.port)
+                sock.sendall(json.dumps({'port': self.listening_port}).encode(encoding))
+                data.sent_id = True
+
+
+if __name__ == "__main__":
+    
+    p = Peer()
+    p.connect_with_seeds()
+    p.run()
--- 1/seed.py
+++ 1/seed.py
@@ -9,38 +9,63 @@ import argparse
 from socket import *
 from utils import Connection, socket_type
 import selectors
-sel = selectors.DefaultSelector()
-
+# sel = selectors.DefaultSelector()
 encoding = 'utf-8'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ip', help='ip address its running on', required=True)
-parser.add_argument('--port', help='port number its running on', required=True)
+parser.add_argument('--ip', help='ip address its running on', type=str, required=True)
+parser.add_argument('--port', help='port number its running on', type=int, required=True)
 
-args = parser.parse_args()
+# port = int(args.port)
+# ip = str(args.ip)  # TODO This should be empty while submitting -?
 
-port = int(args.port)
-ip = str(args.ip)  # TODO This should be empty while submitting
-
-peer_list = []
+# peer_list = []
 
 selector_mask = selectors.EVENT_READ | selectors.EVENT_WRITE
 
+class Seed:
+    def __init__(self, ip, port):
+        super().__init__()
+        self.ip = ip
+        self.port = port
+        self.peer_list = []
+        self.sel = selectors.DefaultSelector()
+        
+    def run(self):
+        # set-up listening socket for selector
+        listener_socket = socket(AF_INET, SOCK_STREAM)
+        # listens on a socket even if previously occupied
+        listener_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        listener_socket.bind((self.ip, self.port))
+        listener_socket.listen(10)
+        listener_socket.setblocking(False)
+        self.sel.register(listener_socket, selectors.EVENT_READ, data=Connection(
+            listener_socket, self.ip, self.port, socket_type.SELF))
+        print("set up listening socket")
+        while True:
+            events = self.sel.select(timeout=None)
+            # print("found events")
+            for key, mask in events:
+                # print("found key", mask)
+                if key.data.type == socket_type.SELF:  # New Peer connection
+                    self.accept_peer(key.fileobj)
+                else:
+                    self.service_connection(key, mask)
+        listener_socket.close()
 
-def parse_message(message):
-    pass
+    def parse_message(self, message):
+        pass
 
-
-def accept_peer(sock):
-    """
-    Accept connection from peer and add it to selector.
-    Dont send peer list as we haven't received listening port yet
-    """
-    peer, (peer_ip, peer_port) = sock.accept()
-    print("Received connection from", peer_ip, peer_port)
-    peer.setblocking(False)
-    sel.register(peer, selector_mask,
-                 data=Connection(peer, peer_ip, None))
+    def accept_peer(self, sock):
+        """
+        Accept connection from peer and add it to selector.
+        Dont send peer list as we haven't received listening port yet
+        """
+        peer, (peer_ip, peer_port) = sock.accept()
+        print("Received connection from", peer_ip, peer_port)
+        peer.setblocking(False)
+        self.sel.register(peer, selector_mask,
+                    data=Connection(peer, peer_ip, None))
 
     # # Might need to ensure you dont send a peer to itself
     # pretty_peers = [connection.pretty() for connection in peer_list]
@@ -49,69 +74,68 @@ def accept_peer(sock):
     # # Not sure how sendall works with select call. Definite way is to use send repeatedly
     # peer.sendall(json.dumps(pretty_peers).encode(encoding))
     # print("Sent peer list")
-
     # peer_list.append(conn) Do this later
 
-
-def service_connection(key, mask):
-    """
-    Handle all requests.
-    Cases:
-        - A peer has sent listening port. Send current peer list and add peer to peer list
-        - A peer has sent dead node info. Remove peer from peer list
-    """
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        try:  # Find a better place for this
-            recv_data = sock.recv(1024).decode(encoding)
-            print(recv_data)
-            if not recv_data:
-                print("closing connection to", data.ip, ":", data.port)
-                sel.unregister(sock)
+    def service_connection(self, key, mask):
+        """
+        Handle all requests.
+        Cases:
+            - A peer has sent listening port. Send current peer list and add peer to peer list
+            - A peer has sent dead node info. Remove peer from peer list
+        """
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            try:  # Find a better place for this
+                recv_data = sock.recv(1024).decode(encoding)
+                print(recv_data)
+                if not recv_data:
+                    print("closing connection to", data.ip, ":", data.port)
+                    self.sel.unregister(sock)
+                    sock.close()
+                elif data.port is None:  # peer has sent listening port
+                    id = json.loads(recv_data)
+                    print("received listening port info", id)
+                    pretty_peers = [connection.pretty()
+                                    for connection in self.peer_list]
+                    sock.sendall(json.dumps(pretty_peers).encode(encoding))
+                    key.data.port = id['port']
+                    self.peer_list.append(key.data)
+                else:  # dead node info
+                    print("received from", data.ip, ":", data.port, ":", recv_data)
+                    parse_message(recv_data)
+            except Exception as e:
+                print(e)
+                self.sel.unregister(sock)
                 sock.close()
-            elif data.port is None:  # peer has sent listening port
-                id = json.loads(recv_data)
-                print("received listening port info", id)
-                pretty_peers = [connection.pretty()
-                                for connection in peer_list]
-                sock.sendall(json.dumps(pretty_peers).encode(encoding))
-                key.data.port = id['port']
-                peer_list.append(key.data)
 
-            else:  # dead node info
-                print("received from", data.ip, ":", data.port, ":", recv_data)
-                parse_message(recv_data)
-        except Exception as e:
-            print(e)
-            sel.unregister(sock)
-            sock.close()
-
-    if mask & selectors.EVENT_WRITE:
-        pass
+        if mask & selectors.EVENT_WRITE:
+            pass
         # print("ready to write on ", key.data.ip, key.data.port)
 
 
-listener_socket = socket(AF_INET, SOCK_STREAM)
-# listens on a socket even if previously occupied
-listener_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-listener_socket.bind((ip, port))
-listener_socket.listen(10)
-listener_socket.setblocking(False)
-sel.register(listener_socket, selectors.EVENT_READ, data=Connection(
-    listener_socket, ip, port, socket_type.SELF))
-print("set up listening socket")
+# listener_socket = socket(AF_INET, SOCK_STREAM)
+# # listens on a socket even if previously occupied
+# listener_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+# listener_socket.bind((ip, port))
+# listener_socket.listen(10)
+# listener_socket.setblocking(False)
+# sel.register(listener_socket, selectors.EVENT_READ, data=Connection(
+#     listener_socket, ip, port, socket_type.SELF))
+# print("set up listening socket")
 
-while True:
+# while True:
 
-    events = sel.select(timeout=None)
-    # print("found events")
-    for key, mask in events:
-        # print("found key", mask)
-        if key.data.type == socket_type.SELF:  # New Peer connection
-            accept_peer(key.fileobj)
-        else:
-            service_connection(key, mask)
-
-
-listener_socket.close()
+#     events = sel.select(timeout=None)
+#     # print("found events")
+#     for key, mask in events:
+#         # print("found key", mask)
+#         if key.data.type == socket_type.SELF:  # New Peer connection
+#             accept_peer(key.fileobj)
+#         else:
+#             service_connection(key, mask)
+# listener_socket.close()
+if __name__ == "__main__":
+    args = parser.parse_args()
+    s = Seed(args.ip, args.port)
+    s.run()
