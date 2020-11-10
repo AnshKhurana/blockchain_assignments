49e76411b70671f6962a96f334c4cc94fbf02417
oddgr8 <onkardeshpande07@gmail.com>
2020-11-10 21:35:43 +0530

    Second experiment

--- 2/wrapper.py
+++ 2/wrapper.py
@@ -2,10 +2,12 @@ import argparse
 import os
 import subprocess
 
+
 def run_all():
-    for fp in [10,20,30]:
-        for iat in [2,4,8,16,32]:
-            out = subprocess.call('python run_experiments.py --nd 2 --iat {} --flood_percentage {} --runtime 12'.format(iat, fp), shell=True)
+    for fp in [10, 20, 30]:
+        for iat in [1, 2, 4, 8, 12]:
+            out = subprocess.call(
+                'python run_experiments.py --nd 1 --iat {} --flood_percentage {} --runtime 12'.format(iat, fp), shell=True)
 
 
 if __name__ == "__main__":
